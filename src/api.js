c3bcbffa987a36fb4394eb81edc9fef6352f3adf
mahadevprsd7 <mahadevprsd7@gmail.com>
2025-06-21 10:52:25 +0530

    api.js is updated with rener url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'http://localhost:5000/api/todos' 
+ baseURL: 'https://todo-backend-2f6f.onrender.com/api/todos' 
 });
 
 export default api;
